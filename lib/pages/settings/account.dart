96871b2732a5266d356d7bcd24d492a07155ce19
Kartik Nayak <thisiskartiknayak@gmail.com>
2023-07-10 15:12:38 +0530

    Modify file account.dart

--- lib/pages/settings/account.dart
+++ lib/pages/settings/account.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../constants.dart';
 import '../../widgets/custom_back_button.dart';
+import '../../widgets/account/account_card.dart';
+import '../../widgets/settings/setting_tile.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
@@ -9,13 +12,36 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
           child: Column(
             children: [
-              CustomBackButton(title: 'AccountPage'),
+              const CustomBackButton(title: 'Account'),
+              const AccountCard(
+                icon: Icons.book,
+                accountType: 'Customer',
+                paymentType: 'R',
+                amount: '750',
+              ),
+              const AccountCard(
+                icon: Icons.local_shipping_sharp,
+                accountType: 'Supplier',
+                paymentType: 'G',
+                amount: '200',
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                  border: Border.all(color: kHighLightColor),
+                ),
+                child: const SettingTile(
+                  icon: Icons.download,
+                  title: 'Download Backup',
+                  hideDivider: true,
+                ),
+              )
             ],
           ),
         ),
